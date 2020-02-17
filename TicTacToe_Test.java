36c9c4eb0440442558b58233bf220fc7a0aab6b4
Marcus <marsjak16@gmail.com>
2020-02-17 14:38:34 -0600

    Made class to test tic tac toe game

---
+++ TicTacToe_Test.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class TicTacToe_Test {
+	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe();
+		//int[][] board = game.getBoard();
+		//String boardStr;
+		Scanner s = new Scanner(System.in);
+		int spot;
+		int x, y;
+		int gameStatus = 0;
+		
+		while(gameStatus == 0) {
+			System.out.println(game.getBoardStr());
+			System.out.println("Player " + game.getTurn() + " where would you like to play? (spot 1-9) ");
+			spot = s.nextInt();
+			spot--;
+			x = spot % 3;
+			y = spot / 3;
+			
+			game.move(game.getTurn(), x, y);
+			gameStatus = game.gameOver();
+		}
+		
+		
+		if(gameStatus == 1) {
+			System.out.println("GAME OVER! Player " + game.whoWon() + " won!");
+		}
+		else {
+			System.out.println("The game is a tie!");
+		}
+			
+		s.close();
+	}
+}
