30f5217f988d3ea4682266c85984e2ebfa5424af
Marcus <marsjak16@gmail.com>
2020-04-27 14:04:38 -0500

    added winning borders to tic tac toe. Also restructed TTT and ConnectFour GUIs for effieciency

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -4,6 +4,7 @@ public class TicTacToe {
 	
 	private int turn;
 	private char[][] board = new char[3][3];
+	private boolean[][] highlights = new boolean[3][3];
 	
 	public TicTacToe() {
 		turn = 1;
@@ -46,36 +47,114 @@ public class TicTacToe {
 	}
 	
 	public int gameOver() {
-		if((board[0][0] == board[0][1] & board[0][1] == board[0][2] & board[0][2] != '*') |
-			(board[1][0] == board[1][1] & board[1][1] == board[1][2] & board[1][2] != '*') | 
-			(board[2][0] == board[2][1] & board[2][1] == board[2][2] & board[2][2] != '*') |
-			(board[0][0] == board[1][0] & board[1][0] == board[2][0] & board[2][0] != '*') | 
-			(board[0][1] == board[1][1] & board[1][1] == board[2][1] & board[2][1] != '*') | 
-			(board[0][2] == board[1][2] & board[1][2] == board[2][2] & board[2][2] != '*') |
-			(board[0][0] == board[1][1] & board[1][1] == board[2][2] & board[2][2] != '*') |
-			(board[0][2] == board[1][1] & board[1][1] == board[2][0] & board[2][0] != '*')) {
-			
-			return 1;
-		}
+		char c;
 		
-		else {
-			// checking for CAT game
-			int numEmpty = 0;
-			for(int i = 0; i < board.length; i++) {
-				for(int j = 0; j < board[i].length; j++) {
-					if(board[i][j] == '*') {
-						numEmpty++;
+		//check horizontal
+		for(int i = 0; i < board.length; i++) {
+			if(board[i][0] != '*') {
+				c = board[i][0];
+				int count = 0;
+				
+				for(int j = 0; j < board[0].length; j++) {
+					if(board[i][j] == c) {
+						count++;
 					}
 				}
-			}
-			if(numEmpty == 0) {
-				return 2;
-			}
-			else {
-				// else game not ended
-			return 0;
+				
+				if(count == 3) {
+					highlightWin(i, 0, 1);
+					return 1;
+				}
 			}
 		}
+		
+		//check vertial
+		for(int j = 0; j < board[0].length; j++) {
+			if(board[0][j] != '*') {
+				c = board[0][j];
+				int count = 0;
+				
+				for(int i = 0; i < board.length; i++) {
+					if(board[i][j] == c) {
+						count++;
+					}
+				}
+				
+				if(count == 3) {
+					highlightWin(0, j, 2);
+					return 1;
+				}
+			}
+		}
+		
+		//check diagonal
+		if(board[0][0] != '*') {
+			c = board[0][0];
+			int count = 0;
+			for(int k = 0; k < 3; k++) {
+				if(board[k][k] == c) {
+					count++;
+				}
+			}
+			if(count == 3) {
+				highlightWin(0, 0, 3);
+				return 1;
+			}
+		}
+			
+		//check other diagonal
+		if(board[0][2] != '*') {
+			c = board[0][2];
+			int count = 0;
+			for(int k = 0; k < 3; k++) {
+				if(board[k][2-k] == c) {
+					count++;
+				}
+			}
+			if(count == 3) {
+				highlightWin(0, 2, 4);
+				return 1;
+			}
+		}
+		
+		// checking for CAT game
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[i].length; j++) {
+				if(board[i][j] == '*') {
+					return 0;
+				}
+			}
+		}
+		
+		// tied CAT game
+		return 2;
+	}
+	
+	private void highlightWin(int x, int y, int dir) {
+		if(dir == 1) {
+			for(int i = 0; i < 3; i++) {
+				highlights[x][y+i] = true;
+			}
+		}
+		else if(dir == 2) {
+			for(int i = 0; i < 3; i++) {
+				highlights[x+i][y] = true;
+			}
+		}
+		else if(dir == 3) {
+			for(int i = 0; i < 3; i++) {
+				highlights[x+i][y+i] = true;
+			}
+		}
+		else if(dir == 4) {
+			for(int i = 0; i < 3; i++) {
+				highlights[x+i][y-i] = true;
+			}
+		}
+	}
+	
+	public boolean getHighlight(int x, int y) {
+		return highlights[x][y];
 	}
 	
 	public int whoWon() {
--- src/tictactoe/TicTacToe_GUI.java
+++ src/tictactoe/TicTacToe_GUI.java
@@ -3,11 +3,12 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class TicTacToe_GUI {
 
 	JFrame frame;
-	JButton button1, button2, button3, button4, button5, button6, button7, button8, button9;
+	JButton[][] gameButtons;
 	JLabel label;
 	JPanel panel1, panel2;
 	int turn;
@@ -26,69 +27,32 @@ public class TicTacToe_GUI {
 		label = new JLabel("Player " + turn + " it is your turn");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		button1 = new JButton("Button 1");
-		button2 = new JButton("Button 2");
-		button3 = new JButton("Button 3");
-		button4 = new JButton("Button 4");
-		button5 = new JButton("Button 5");
-		button6 = new JButton("Button 6");
-		button7 = new JButton("Button 7");
-		button8 = new JButton("Button 8");
-		button9 = new JButton("Button 9");
+		gameButtons = new JButton[3][3];
 		
-		button1.setName("1");
-		button2.setName("2");
-		button3.setName("3");
-		button4.setName("4");
-		button5.setName("5");
-		button6.setName("6");
-		button7.setName("7");
-		button8.setName("8");
-		button9.setName("9");
-		
+		int spot = 1;
+		for(int i = 0; i < gameButtons.length; i++) {
+			for(int j = 0; j < gameButtons[0].length; j++) {
+				gameButtons[i][j] = new JButton("");
+				gameButtons[i][j].setName(spot + "");
+				spot++;
+				gameButtons[i][j].setFont(new Font("Arial", Font.PLAIN, 70));
+				gameButtons[i][j].addActionListener(new ButtonActionListener());
+			}
+		}
 		
 		updateBoard();
-		
-		button1.setFont(new Font("Arial", Font.PLAIN, 70));
-		button2.setFont(new Font("Arial", Font.PLAIN, 70));
-		button3.setFont(new Font("Arial", Font.PLAIN, 70));
-		button4.setFont(new Font("Arial", Font.PLAIN, 70));
-		button5.setFont(new Font("Arial", Font.PLAIN, 70));
-		button6.setFont(new Font("Arial", Font.PLAIN, 70));
-		button7.setFont(new Font("Arial", Font.PLAIN, 70));
-		button8.setFont(new Font("Arial", Font.PLAIN, 70));
-		button9.setFont(new Font("Arial", Font.PLAIN, 70));
-		
-		button1.addActionListener(new ButtonActionListener());
-		button2.addActionListener(new ButtonActionListener());
-		button3.addActionListener(new ButtonActionListener());
-		button4.addActionListener(new ButtonActionListener());
-		button5.addActionListener(new ButtonActionListener());
-		button6.addActionListener(new ButtonActionListener());
-		button7.addActionListener(new ButtonActionListener());
-		button8.addActionListener(new ButtonActionListener());
-		button9.addActionListener(new ButtonActionListener());
-		
-		
-		
+
 		panel1 = new JPanel();
 		panel2 = new JPanel();
 		panel2.setLayout(new GridLayout(3, 3));
 		
-		panel2.add(button1);
-		panel2.add(button2);
-		panel2.add(button3);
-		panel2.add(button4);
-		panel2.add(button5);
-		panel2.add(button6);
-		panel2.add(button7);
-		panel2.add(button8);
-		panel2.add(button9);
+		for(int i = 0; i < gameButtons.length; i++) {
+			for(int j = 0; j < gameButtons[0].length; j++) {
+				panel2.add(gameButtons[i][j]);
+			}
+		}
 		
 		panel1.add(label);
-		//panel1.add(panel2);
-		
-		//frame.add(panel1);
 		
 		frame.add(panel1, BorderLayout.PAGE_START);
 		frame.add(panel2, BorderLayout.CENTER);
@@ -100,15 +64,11 @@ public class TicTacToe_GUI {
 	}
 	
 	private void updateBoard() {
-		button1.setText(game.getBoardSpot(0, 0));
-		button2.setText(game.getBoardSpot(0, 1));
-		button3.setText(game.getBoardSpot(0, 2));
-		button4.setText(game.getBoardSpot(1, 0));
-		button5.setText(game.getBoardSpot(1, 1));
-		button6.setText(game.getBoardSpot(1, 2));
-		button7.setText(game.getBoardSpot(2, 0));
-		button8.setText(game.getBoardSpot(2, 1));
-		button9.setText(game.getBoardSpot(2, 2));
+		for(int i = 0; i < gameButtons.length; i++) {
+			for(int j = 0; j < gameButtons[0].length; j++) {
+				gameButtons[i][j].setText(game.getBoardSpot(i, j));
+			}
+		}
 	}
 	
 	public class ButtonActionListener implements ActionListener {
@@ -135,6 +95,8 @@ public class TicTacToe_GUI {
 					
 					label.setText("PLAYER " + winner + " HAS WON THE GAME!");
 					
+					setWinHighlight();
+					
 					disableAllButtons();
 				}
 				else if(game.gameOver() == 2) {
@@ -147,15 +109,23 @@ public class TicTacToe_GUI {
 		}
 	}
 	
+	private void setWinHighlight() {
+		Border border = BorderFactory.createLineBorder(Color.blue, 5);
+		
+		for(int i = 0; i < gameButtons.length; i++) {
+			for(int j = 0; j < gameButtons[0].length; j++) {
+				if(game.getHighlight(i, j)) {
+					gameButtons[i][j].setBorder(border);
+				}
+			}
+		}
+	}
+	
 	private void disableAllButtons() {
-		button1.setEnabled(false);
-		button2.setEnabled(false);
-		button3.setEnabled(false);
-		button4.setEnabled(false);
-		button5.setEnabled(false);
-		button6.setEnabled(false);
-		button7.setEnabled(false);
-		button8.setEnabled(false);
-		button9.setEnabled(false);
+		for(int i = 0; i < gameButtons.length; i++) {
+			for(int j = 0; j < gameButtons[0].length; j++) {
+				gameButtons[i][j].setEnabled(false);
+			}
+		}
 	}
 }
